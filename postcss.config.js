a46551c8c1e9cf4ad8b501bdd1d58589483c0f26
RobynAwesome <rkholofelo@gmail.com>
2026-03-20 07:57:49 +0200

    ified

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,3 @@
-// postcss.config.js
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    autoprefixer: {},
-  },
-};
+(node:23236) Warning: Failed to load the ES module: C:\Users\rkhol\Portfolio\postcss.config.js. Make sure to set "type": "module" in the nearest package.json file or use the .mjs extension.
+(Use `node --trace-warnings ...` to show where the warning was created)
+
